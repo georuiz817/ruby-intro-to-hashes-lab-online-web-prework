4a31053f0e05edb2208a422a979bdb9422bce701
George Ruiz <georuiz817@gmail.com>
2019-06-09 04:19:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,9 +13,9 @@ monopoly = {:railroads => {}
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads => {:pieces => 4}, {:name => {}},{:rent_in_dollars => {}}
+	monopoly = {:railroads => {:pieces => 4}, {:name => {}}, {:rent_in_dollars => {}}
 }
-}
+
 end
 
 def monopoly_with_third_tier
